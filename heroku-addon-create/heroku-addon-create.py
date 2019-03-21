64ddb697764980ec78b9cd5e0376efa4b735596f
Aaron Chu <aaron.chu@therealreal.com>
2019-03-21 09:01:58 -0700

    remove commented code

--- heroku-addon-create/heroku-addon-create.py
+++ heroku-addon-create/heroku-addon-create.py
@@ -55,7 +55,6 @@ def get_review_app_by_branch( pipeline_id, branch_name ):
 def get_app_by_name( app_name ):
     r = requests.get(API_URL_HEROKU+'/apps', headers=HEADERS_HEROKU)
     apps = json.loads(r.text)
-#    print(json.dumps(apps, sort_keys=True, indent=4))
     app = next((x for x in apps if x['name'] == app_name), None)
     try:
         if app is not None and 'id' in app:
