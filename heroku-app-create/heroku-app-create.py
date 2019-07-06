f1cbdccfe8beb91143e049a9c0484d252877eba7
Aaron Chu <aaron.chu@therealreal.com>
2019-07-05 20:54:35 -0700

    try another format

--- heroku-app-create/heroku-app-create.py
+++ heroku-app-create/heroku-app-create.py
@@ -27,14 +27,14 @@ HEADERS_HEROKU = {
     'Authorization': 'Bearer %s' % HEROKU_TOKEN,
     'User-Agent': 'Heroku GitHub Actions Provider by TheRealReal',
     'Content-Type': 'application/json',
-    'Range': 'max=20'
+    'Range': 'id ..; max=20;'
     }
 HEADERS_HEROKU_REVIEW_PIPELINES = {
     'Accept': 'application/vnd.heroku+json; version=3.pipelines',
     'Authorization': 'Bearer %s' % HEROKU_TOKEN,
     'User-Agent': 'Heroku GitHub Actions Provider by TheRealReal',
     'Content-Type': 'application/json',
-    'Range': 'max=20'
+    'Range': 'id ..; max=20;'
     }
 
 API_URL_HEROKU = 'https://api.heroku.com'
