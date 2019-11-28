94223782e76a25674c75b573d6ba970177f1c800
Derek Sweet <dereksweet@gmail.com>
2019-11-27 21:57:28 -0700

    debug output redirect uris

--- okta-whitelist-url-destroy/okta-whitelist-url-destroy.py
+++ okta-whitelist-url-destroy/okta-whitelist-url-destroy.py
@@ -131,6 +131,7 @@ r = requests.get(okta_api_url, headers=headers_okta)
 client = json.loads(r.text)
 
 redirect_uris = client['redirect_uris']
+print("Redirect URIs: %s" % ( redirect_uris ))
 
 if any(uri_to_remove in s for s in redirect_uris):
   print ('The URI %s is whitelisted. Removing it from the whitelist now!' % uri_to_remove)
